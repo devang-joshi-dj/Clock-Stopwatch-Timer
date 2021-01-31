9543d52a9d95c75b54b9cedd98e9741d8499b59e
Devang Joshi <devangjoshi11@gmail.com>
2021-01-31 18:54:47 +0530

    Done with Timer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const time = () => {
   // function for getting the hour, minute, second, date, month, day-name, year and printing them
   var date = new Date();
@@ -61,6 +63,8 @@ window.onload = () => {
 
   document.getElementById("stopwatchReset").disabled = true; // reset button of Stopwatch disabled on page load
   document.getElementById("stopwatchCountLap").disabled = true; // lap button of Stopwatch disabled on page load
+  document.getElementById("timerReset").disabled = true; // reset button of Timer disabled on page load
+  document.getElementById("timerDelete").disabled = true; // delete button of Timer disabled on page load
 
   document.getElementById("clockHeading").onclick = () => {
     // following code removes class-name from the elements. Following approach is taken so it can work in all the browsers
@@ -132,15 +136,29 @@ window.onload = () => {
   document.getElementById("stopwatchStartStop").onclick = () => {
     if (document.getElementById("stopwatchStartStop").innerHTML == "START") {
       startStopwatch();
-    } else if (
-      document.getElementById("stopwatchStartStop").innerHTML == "STOP"
-    ) {
+    } else if (document.getElementById("stopwatchStartStop").innerHTML == "STOP") {
       stopStopwatch();
     }
   };
 
   document.getElementById("stopwatchReset").onclick = () => resetStopwatch();
   document.getElementById("stopwatchCountLap").onclick = () => createLap();
+
+
+  document.getElementById("timerHour").value = "";
+  document.getElementById("timerMinute").value = "";
+  document.getElementById("timerSecond").value = "";
+
+  document.getElementById("timerStartStop").onclick = () => {
+    if (document.getElementById("timerStartStop").innerHTML == "START") {
+      startTimer();
+    } else if (document.getElementById("timerStartStop").innerHTML == "STOP") {
+      stopTimer();
+    }
+  };
+
+  document.getElementById("timerReset").onclick = () => resetTimer();
+  document.getElementById("timerDelete").onclick = () => deleteTimer();
 };
 
 var controlStopwatch;
@@ -149,9 +167,7 @@ var intervalID = null;
 var stopwatchHour = Number(document.getElementById("stopwatchHour"));
 var stopwatchMinute = Number(document.getElementById("stopwatchMinute"));
 var stopwatchSecond = Number(document.getElementById("stopwatchSecond"));
-var stopwatchMillisecond = Number(
-  document.getElementById("stopwatchMillisecond")
-);
+var stopwatchMillisecond = Number(document.getElementById("stopwatchMillisecond"));
 
 function controlStopwatch() {
   stopwatchMillisecond += 1;
@@ -167,20 +183,13 @@ function controlStopwatch() {
     stopwatchHour += 1;
     stopwatchMinute = 0;
   }
-  if (
-    stopwatchHour == 23 &&
-    stopwatchMinute == 59 &&
-    stopwatchSecond == 59 &&
-    stopwatchMillisecond == 59
-  ) {
+  if (stopwatchHour == 23 && stopwatchMinute == 59 && stopwatchSecond == 59 && stopwatchMillisecond == 59) {
     stopStopwatch();
     document.getElementById("stopwatchStartStop").disabled = true;
     document.getElementById("stopwatchPause").disabled = true;
     document.getElementById("stopwatchReset").disabled = false;
   }
-  document.getElementById(
-    "stopwatchMillisecond"
-  ).innerHTML = stopwatchMillisecond;
+  document.getElementById("stopwatchMillisecond").innerHTML = stopwatchMillisecond;
   document.getElementById("stopwatchSecond").innerHTML = stopwatchSecond;
   document.getElementById("stopwatchMinute").innerHTML = stopwatchMinute;
   document.getElementById("stopwatchHour").innerHTML = stopwatchHour;
@@ -219,9 +228,7 @@ function resetStopwatch() {
   document.getElementById("stopwatchHour").innerHTML = stopwatchHour;
   document.getElementById("stopwatchMinute").innerHTML = stopwatchMinute;
   document.getElementById("stopwatchSecond").innerHTML = stopwatchSecond;
-  document.getElementById(
-    "stopwatchMillisecond"
-  ).innerHTML = stopwatchMillisecond;
+  document.getElementById("stopwatchMillisecond").innerHTML = stopwatchMillisecond;
   document.getElementById("lap").innerHTML = "";
   var parent = document.getElementsByClassName("stopwatchButton")[0];
   var child = document.getElementsByClassName("lapWarning")[0];
@@ -241,15 +248,7 @@ function createLap() {
     hash.appendChild(hashContent);
     var lap = document.createElement("span");
     lap.className = "lapTime";
-    var lapContent = document.createTextNode(
-      stopwatchHour +
-        ":" +
-        stopwatchMinute +
-        ":" +
-        stopwatchSecond +
-        ":" +
-        stopwatchMillisecond
-    );
+    var lapContent = document.createTextNode(stopwatchHour + ":" + stopwatchMinute + ":" + stopwatchSecond + ":" + stopwatchMillisecond);
     lap.appendChild(lapContent);
     content.appendChild(hash);
     content.appendChild(lap);
@@ -272,8 +271,170 @@ function createLap() {
   }
 }
 
-const timerHourChange = () => {
-  val = event.target.value;
-  console.log(val);
-  document.getElementById("myInput").value = test;
-};
+// let val = "";
+// const timerHourChange = (event) => {
+//   if (event.target.value === '1' || event.target.value === '2' || event.target.value === '3' || event.target.value === '4' || event.target.value === '5' || event.target.value === '6' || event.target.value === '7' || event.target.value === '8' || event.target.value === '9' || event.target.value === '0') {
+//     document.getElementById("timerHour").value += event.target.value;
+//     console.log("true");
+//   }
+//   else {
+//     document.getElementById("timerHour").value += "";
+//     console.log("false");
+//   }
+//   console.log(val);
+// };
+
+// const timerHourChange = () => {
+//   var x = document.getElementById("timerHour").value;
+//   if (x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9' || x == '0') {
+//     document.getElementById("timerHour").value = x;
+//   }
+//   else {
+//     console.log(typeof x);
+//     document.getElementById("timerHour").value = null;
+//   }
+// }
+
+function controlTimer() {
+  const startTime = performance.now();
+  timerMillisecond -= 1;
+  if (timerMillisecond < 0) {
+    timerSecond -= 1;
+    timerMillisecond = timerMillisecondDefaultValue;
+  }
+  if (timerSecond < 0) {
+    timerMinute -= 1;
+    timerSecond = 59;
+  }
+  if (timerMinute < 0) {
+    timerHour -= 1;
+    timerMinute = 59;
+  }
+  if (timerHour == 0 && timerMinute == 0 && timerSecond == 0 && timerMillisecond == 0) {
+    firstStartTimer = true;
+    timerMillisecond = 99;
+    resetTimer();
+  }
+  document.getElementsByClassName("timerSecondDisplay")[0].innerHTML = timerSecond;
+  document.getElementsByClassName("timerMinuteDisplay")[0].innerHTML = timerMinute;
+  document.getElementsByClassName("timerHourDisplay")[0].innerHTML = timerHour;
+  const endTime = performance.now();
+  console.log(endTime - startTime);
+}
+
+var intervalIDTimer = null;
+function intervalManagerTimer(flag, controlTimer, time) {
+  if (flag) intervalIDTimer = setInterval(controlTimer, time);
+  else clearInterval(intervalIDTimer);
+}
+
+var firstStartTimer = true;
+var reset = false;
+var timerHour = 0;
+var timerMinute = 0;
+var timerSecond = 0;
+var timerMillisecond;
+var timerMillisecondDefaultValue = 99;
+var permanentTimerHour;
+var permanentTimerMinute;
+var permanentTimerSecond;
+
+function startTimer() {
+  document.getElementById("timerStartStop").innerHTML = "START";
+  if (firstStartTimer) {
+    var parent = document.getElementsByClassName("timerTime")[0];
+    var child = document.getElementsByClassName("timerContent")[0];
+    if (typeof child != "undefined" && child != null) {
+      parent.removeChild(child);
+    }
+
+    document.getElementById("timerHour").disabled = true;
+    document.getElementById("timerMinute").disabled = true;
+    document.getElementById("timerSecond").disabled = true;
+
+    firstStartTimer = false;
+    timerHour = Number(document.getElementById("timerHour").value);
+    timerMinute = Number(document.getElementById("timerMinute").value);
+    timerSecond = Number(document.getElementById("timerSecond").value);
+    timerMillisecond = 99;
+    permanentTimerHour = timerHour;
+    permanentTimerMinute = timerMinute;
+    permanentTimerSecond = timerSecond;
+    var timerContent = document.createElement("div");
+    timerContent.className = "timerContent";
+
+    var timerHourDisplay = document.createElement("div");
+    timerHourDisplay.className = "timerHourDisplay";
+    var timerHourDisplayContent = document.createTextNode(timerHour);
+    timerHourDisplay.appendChild(timerHourDisplayContent);
+
+    var timerMinuteDisplay = document.createElement("div");
+    timerMinuteDisplay.className = "timerMinuteDisplay";
+    var timerMinuteDisplayContent = document.createTextNode(timerMinute);
+    timerMinuteDisplay.appendChild(timerMinuteDisplayContent);
+
+    var timerSecondDisplay = document.createElement("div");
+    timerSecondDisplay.className = "timerSecondDisplay";
+    var timerSecondDisplayContent = document.createTextNode(timerSecond);
+    timerSecondDisplay.appendChild(timerSecondDisplayContent);
+
+    timerContent.appendChild(timerHourDisplay);
+    timerContent.appendChild(timerMinuteDisplay);
+    timerContent.appendChild(timerSecondDisplay);
+
+    var timerElement = document.getElementsByClassName("timerTime")[0];
+    timerElement.appendChild(timerContent);
+
+  }
+  if (timerHour == 0 && timerMinute == 0 && timerSecond == 0) {
+    document.getElementById("timerStartStop").innerHTML = "START";
+    firstStartTimer = true;
+  }
+  if (reset) {
+    reset = false;
+    timerMillisecond = timerMillisecondDefaultValue;
+    timerHour = permanentTimerHour;
+    timerMinute = permanentTimerMinute;
+    timerSecond = permanentTimerSecond;
+  }
+  if (timerHour !== 0 || timerMinute !== 0 || timerSecond !== 0) {
+    intervalManagerTimer(true, controlTimer, 10);
+    document.getElementById("timerStartStop").innerHTML = "STOP";
+  }
+  document.getElementById("timerReset").disabled = false;
+  document.getElementById("timerDelete").disabled = false;
+}
+
+function stopTimer() {
+  intervalManagerTimer(false);
+  document.getElementById("timerStartStop").innerHTML = "START";
+}
+
+function resetTimer() {
+  reset = true;
+  intervalManagerTimer(false);
+  document.getElementById("timerStartStop").innerHTML = "START";
+  document.getElementsByClassName("timerSecondDisplay")[0].innerHTML = permanentTimerSecond;
+  document.getElementsByClassName("timerMinuteDisplay")[0].innerHTML = permanentTimerMinute;
+  document.getElementsByClassName("timerHourDisplay")[0].innerHTML = permanentTimerHour;
+}
+
+function deleteTimer() {
+  intervalManagerTimer(false);
+  timerMillisecond = timerMillisecondDefaultValue;
+  timerHour = 0;
+  timerMinute = 0;
+  timerSecond = 0;
+  firstStartTimer = true;
+  document.getElementById("timerStartStop").innerHTML = "START";
+  document.getElementById("timerReset").disabled = true;
+  document.getElementById("timerDelete").disabled = true;
+
+  document.getElementById("timerHour").disabled = false;
+  document.getElementById("timerMinute").disabled = false;
+  document.getElementById("timerSecond").disabled = false;
+
+  var parent = document.getElementsByClassName("timerTime")[0];
+  var child = document.getElementsByClassName("timerContent")[0];
+  parent.removeChild(child);
+}
